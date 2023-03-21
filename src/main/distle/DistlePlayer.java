c148caca5ac4f0adcb8dd611011edcf3b9460b23
christina choi <cchoi17@lion.lmu.edu>
2023-03-20 19:37:02 -0700

    completed homework

--- src/main/distle/DistlePlayer.java
+++ src/main/distle/DistlePlayer.java
@@ -8,63 +8,74 @@ import java.util.*;
  * Distle with frightening accuracy (hopefully).
  */
 public class DistlePlayer {
-    
-    // [!] TODO: Any fields you want here!
-    
+    int currGuesses;
+    Set<String> wordList;
+
     /**
      * Constructs a new DistlePlayer.
-     * [!] You MAY NOT change this signature, meaning it may not accept any arguments.
+     * [!] You MAY NOT change this signature, meaning it may not accept any
+     * arguments.
      * Still, you can use this constructor to initialize any fields that need to be,
-     * though you may prefer to do this in the {@link #startNewGame(Set<String> dictionary, int maxGuesses)}
+     * though you may prefer to do this in the {@link #startNewGame(Set<String>
+     * dictionary, int maxGuesses)}
      * method.
      */
-    public DistlePlayer () {
-        // [!] TODO: Any initialization of fields you want here (can also leave empty)
+    public DistlePlayer() {
     }
-    
+
     /**
      * Called at the start of every new game of Distle, and parameterized by the
      * dictionary composing all possible words that can be used as guesses / one of
      * which is the correct.
      * 
      * @param dictionary The dictionary from which the correct answer and guesses
-     * can be drawn.
+     *                   can be drawn.
      * @param maxGuesses The max number of guesses available to the player.
      */
-    public void startNewGame (Set<String> dictionary, int maxGuesses) {
-        // [!] TODO!
-        throw new UnsupportedOperationException();
+    public void startNewGame(Set<String> dictionary, int maxGuesses) {
+        this.currGuesses = maxGuesses;
+        this.wordList = dictionary;
     }
-    
+
     /**
      * Requests a new guess to be made in the current game of Distle. Uses the
      * DistlePlayer's fields to arrive at this decision.
      * 
      * @return The next guess from this DistlePlayer.
      */
-    public String makeGuess () {
-        // [!] TODO!
-        throw new UnsupportedOperationException();
+    public String makeGuess() {
+        Iterator<String> words = this.wordList.iterator();
+        return words.next();
     }
-    
+
     /**
-     * Called by the DistleGame after the DistlePlayer has made an incorrect guess. The
+     * Called by the DistleGame after the DistlePlayer has made an incorrect guess.
+     * The
      * feedback furnished is as follows:
      * <ul>
-     *   <li>guess, the player's incorrect guess (repeated here for convenience)</li>
-     *   <li>editDistance, the numerical edit distance between the guess and secret word</li>
-     *   <li>transforms, a list of top-down transforms needed to turn the guess into the secret word</li>
+     * <li>guess, the player's incorrect guess (repeated here for convenience)</li>
+     * <li>editDistance, the numerical edit distance between the guess and secret
+     * word</li>
+     * <li>transforms, a list of top-down transforms needed to turn the guess into
+     * the secret word</li>
      * </ul>
-     * [!] This method should be used by the DistlePlayer to update its fields and plan for
+     * [!] This method should be used by the DistlePlayer to update its fields and
+     * plan for
      * the next guess to be made.
      * 
-     * @param guess The last, incorrect, guess made by the DistlePlayer
+     * @param guess        The last, incorrect, guess made by the DistlePlayer
      * @param editDistance Numerical distance between the guess and the secret word
-     * @param transforms List of top-down transforms needed to turn the guess into the secret word
+     * @param transforms   List of top-down transforms needed to turn the guess into
+     *                     the secret word
      */
-    public void getFeedback (String guess, int editDistance, List<String> transforms) {
-        // [!] TODO!
-        throw new UnsupportedOperationException();
+    public void getFeedback(String guess, int editDistance, List<String> transforms) {
+        Iterator<String> words = this.wordList.iterator();
+        while (words.hasNext()) {
+            List<String> nextGuess = getTransformationList(guess, words.next());
+            if (!transforms.equals(nextGuess)) {
+                words.remove();
+            }
+        }
     }
-    
+
 }
--- src/main/distle/EditDistanceUtils.java
+++ src/main/distle/EditDistanceUtils.java
@@ -3,7 +3,7 @@ package main.distle;
 import java.util.*;
 
 public class EditDistanceUtils {
-    
+
     /**
      * Returns the completed Edit Distance memoization structure, a 2D array
      * of ints representing the number of string manipulations required to minimally
@@ -13,37 +13,121 @@ public class EditDistanceUtils {
      * @param s1 Target of transformation
      * @return Completed Memoization structure for editDistance(s0, s1)
      */
-    public static int[][] getEditDistTable (String s0, String s1) {
-        // [!] TODO!
-        throw new UnsupportedOperationException();
+    public static int[][] getEditDistTable(String s0, String s1) {
+        int[][] T = new int[s0.length() + 1][s1.length() + 1];
+
+        for (int r = 0; r <= s0.length(); r++) {
+            T[r][0] = r;
+        }
+        for (int c = 0; c <= s1.length(); c++) {
+            T[0][c] = c;
+        }
+        for (int r = 1; r <= s0.length(); r++) {
+            for (int c = 1; c <= s1.length(); c++) {
+                int e = Integer.MAX_VALUE;
+                int f = Integer.MAX_VALUE;
+                int g = Integer.MAX_VALUE;
+                int h = Integer.MAX_VALUE;
+                if (r >= 1) { // deletion case
+                    e = T[r - 1][c] + 1;
+                }
+                if (c >= 1) { // insertion case
+                    f = T[r][c - 1] + 1;
+                }
+                if (r >= 1 && c >= 1) { // replacement case
+                    g = T[r - 1][c - 1] + (s1.charAt(c - 1) != s0.charAt(r - 1) ? 1 : 0);
+                }
+                if (r >= 2 && c >= 2 && (s0.charAt(r - 1) == s1.charAt(c - 2))
+                        && (s0.charAt(r - 2) == s1.charAt(c - 1))) { // transposition case
+                    h = T[r - 2][c - 2] + 1;
+                }
+                T[r][c] = Math.min(Math.min(e, f), Math.min(g, h));
+            }
+        }
+        return T;
     }
-    
+
     /**
      * Returns one possible sequence of transformations that turns String s0
      * into s1. The list is in top-down order (i.e., starting from the largest
      * subproblem in the memoization structure) and consists of Strings representing
      * the String manipulations of:
      * <ol>
-     *   <li>"R" = Replacement</li>
-     *   <li>"T" = Transposition</li>
-     *   <li>"I" = Insertion</li>
-     *   <li>"D" = Deletion</li>
+     * <li>"R" = Replacement</li>
+     * <li>"T" = Transposition</li>
+     * <li>"I" = Insertion</li>
+     * <li>"D" = Deletion</li>
      * </ol>
      * In case of multiple minimal edit distance sequences, returns a list with
      * ties in manipulations broken by the order listed above (i.e., replacements
-     * preferred over transpositions, which in turn are preferred over insertions, etc.)
-     * @param s0 String transforming into other
-     * @param s1 Target of transformation
-     * @param table Precomputed memoization structure for edit distance between s0, s1
+     * preferred over transpositions, which in turn are preferred over insertions,
+     * etc.)
+     * 
+     * @param s0    String transforming into other
+     * @param s1    Target of transformation
+     * @param table Precomputed memoization structure for edit distance between s0,
+     *              s1
      * @return List that represents a top-down sequence of manipulations required to
-     * turn s0 into s1, e.g., ["R", "R", "T", "I"] would be two replacements followed
-     * by a transposition, then insertion.
+     *         turn s0 into s1, e.g., ["R", "R", "T", "I"] would be two replacements
+     *         followed
+     *         by a transposition, then insertion.
      */
-    public static List<String> getTransformationList (String s0, String s1, int[][] table) {
-        // [!] TODO!
-        throw new UnsupportedOperationException();
+    public static List<String> getTransformationList(String s0, String s1, int[][] table) {
+        List<String> minimalEdits = new ArrayList<String>();
+        int currPos = table[s0.length()][s1.length()];
+        int r = s0.length();
+        int c = s1.length();
+        while (r != 0 || c != 0) {
+            int transposition = 0;
+            int replacement = 0;
+            int insertion = 0;
+            int deletion = 0;
+            if (r >= 1) {
+                deletion = table[r - 1][c] + 1;
+            }
+            if (c >= 1) {
+                insertion = table[r][c - 1] + 1;
+            }
+            if (c >= 1 && r >= 1) {
+                if (s1.charAt(c - 1) != s0.charAt(r - 1)) {
+                    replacement = table[r - 1][c - 1] + 1;
+                    if (replacement == currPos) {
+                        minimalEdits.add("R");
+                        currPos = table[r - 1][c - 1];
+                        r--;
+                        c--;
+                    }
+                } else if (s1.charAt(c - 1) == s0.charAt(r - 1)) {
+                    replacement = table[r - 1][c - 1];
+                    if (replacement == currPos) {
+                        currPos = table[r - 1][c - 1];
+                        r--;
+                        c--;
+                    }
+                }
+            }
+            if (c >= 2 && r >= 2 && (s0.charAt(r - 1) == s1.charAt(c - 2)) && (s0.charAt(r - 2) == s1.charAt(c - 1))) {
+                transposition = table[r - 2][c - 2] + 1;
+                if (transposition == currPos) {
+                    minimalEdits.add("T");
+                    currPos = table[r - 2][c - 2];
+                    r -= 2;
+                    c -= 2;
+                }
+            }
+            if (insertion == currPos) {
+                minimalEdits.add("I");
+                currPos = table[r][c - 1];
+                c--;
+            } else if (deletion == currPos) {
+                minimalEdits.add("D");
+                currPos = table[r - 1][c];
+                r--;
+            }
+        }
+        return minimalEdits;
     }
-    
+
     /**
      * Returns the edit distance between the two given strings: an int
      * representing the number of String manipulations (Insertions, Deletions,
@@ -54,15 +138,17 @@ public class EditDistanceUtils {
      * @param s1 Target of transformation
      * @return The minimal number of manipulations required to turn s0 into s1
      */
-    public static int editDistance (String s0, String s1) {
-        if (s0.equals(s1)) { return 0; }
+    public static int editDistance(String s0, String s1) {
+        if (s0.equals(s1)) {
+            return 0;
+        }
         return getEditDistTable(s0, s1)[s0.length()][s1.length()];
     }
-    
+
     /**
      * See {@link #getTransformationList(String s0, String s1, int[][] table)}.
      */
-    public static List<String> getTransformationList (String s0, String s1) {
+    public static List<String> getTransformationList(String s0, String s1) {
         return getTransformationList(s0, s1, getEditDistTable(s0, s1));
     }
 
